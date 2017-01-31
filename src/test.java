d12ab0adadd3b720dcf03ce29ad21f460bc75a79
John E Krause <jekrause@uwm.edu>
2017-01-31 14:39:22 -0600

    Test

--- src/test.java
+++ src/test.java
@@ -2,6 +2,7 @@
 public class test {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
+		System.out.println("test");
 		int i = 0;
 		while(i < 10){
 			System.out.println(i);
