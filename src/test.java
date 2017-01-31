cbc2bfa9c26f41283c634781cf47b3bdd3c3a350
ed <ed@localhost>
2017-01-31 14:47:23 -0600

    Try

--- src/test.java
+++ src/test.java
@@ -4,6 +4,7 @@ public class test {
 		System.out.println("Hello World!");
 		System.out.println("test");
 		System.out.println("ok?");
+		System.out.println("ok?");
 		int i = 0;
 		while(i < 10){
 			System.out.println(i);
