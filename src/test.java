8ae8f53d385b01ec16961fe01fadc4449b38a519
dishenwen <dwen@uwm.edu>
2017-01-31 14:43:01 -0600

    HI

--- src/test.java
+++ src/test.java
@@ -3,6 +3,7 @@ public class test {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("test");
+		System.out.println("ok?");
 		int i = 0;
 		while(i < 10){
 			System.out.println(i);
