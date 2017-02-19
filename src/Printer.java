122d42780fb9320955236d4f3c5285604b34d2e4
KRAUSE1 <krausedev@gmail.com>
2017-02-19 14:26:47 -0600

    added comparator to Printer.class to order finish times

--- src/Printer.java
+++ src/Printer.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Printer {
@@ -11,18 +13,19 @@ public class Printer {
 		//print data line for each player 
 		BufferedWriter output;
 		Player tempP;
+		int place = 1;
 		try {
 			//write to file
 			output = new BufferedWriter(new FileWriter (file));
-			
+			p = sortByTime(p);
 			for(int i =0; i <p.size(); i++){
 				tempP = p.get(i);
-				if(tempP.DNF){
-					output.write("ID:"+tempP.getID() + "\t start:" + tempP.startTime + "\t end:DNF"  + "\t place:" + (i+1) + "\n");
-
+				if(!tempP.DNF){
+					output.write("ID:"+tempP.getID() + "\t start:" + tempP.startTime + "\t end:" + tempP.endTime +   "\t elapsed time: "+ tempP.totalTime + "\t place:" + place + "\n");
+					place++;
 				}
 				else{
-					output.write("ID:"+tempP.getID() + "\t start:" + tempP.startTime + "\t end:" + tempP.endTime + "\t place:" + (i+1) + "\n");
+					output.write("ID:"+tempP.getID() + "\t start:" + tempP.startTime + "\t end:DNF" + "\n");
 				}
 			}
 			
@@ -35,8 +38,16 @@ public class Printer {
 	}
 	
 	public List<Player> sortByTime(List<Player> p){
-		List<Player> sortedList = new ArrayList<>();
-		
+		List<Player> sortedList = new ArrayList<>(p.size());
+		sortedList.addAll(p);
+	    
+		sortedList.sort(timeComparator);
 		return sortedList;
 	}
+	
+	public static Comparator<Player> timeComparator = new Comparator<Player>(){
+		public int compare(Player p1,Player p2){
+			return (int) (p1.totalTime - p2.totalTime);
+		}
+	};
 }
