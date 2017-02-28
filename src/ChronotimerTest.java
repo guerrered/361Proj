1dc5afd4fbe6f13fa5d1b604cb3dda36b78515c8
KRAUSE1 <krausedev@gmail.com>
2017-02-28 13:07:13 -0600

    Started JUnit test class

---
+++ src/ChronotimerTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import Event.RaceIndependent;
+
+public class ChronotimerTest {
+	RaceIndependent rIND;
+	@Before
+	public void setup() {
+		rIND = new RaceIndependent();
+	}
+	
+	@Test
+	public void testRemovePlayer(){
+		assertTrue(rIND.removeRunner(0));
+	}
+	
+	@Test
+	public void testRemovePlayerTwice(){
+		assertTrue(rIND.removeRunner(0));
+		assertFalse(rIND.removeRunner(0));
+	}
+	
+	@Test
+	public void testSwapPlayers(){
+		//swap should fail if neither are running
+		assertFalse(rIND.swapRacers(0, 1));
+		//start 0
+		rIND.startIND(System.currentTimeMillis());
+		//start 1
+		rIND.startIND(System.currentTimeMillis());
+		
+		assertTrue(rIND.swapRacers(0, 1));
+	}
+
+}
